e9ec03e1caa7c417fbb7f41754585825502893d1
Ivan Vučica <ivan@vucica.net>
2007-10-23 18:57:59 +0000

    removed code from GLICT/glut-helper.* to make compile instructions simpler

--- glict/GLICT/glut-helper.h
+++ glict/GLICT/glut-helper.h
@@ -21,25 +21,3 @@
 
 /* This file is now defunct. Its functionality is now superseded by fonts.h */
 
-
-#error GLUT-HELPER.H is defunct, use FONTS.H for similar functionality.
-
-
-// Glut-helper.cpp
-// contains glut helper
-
-// objnet glut helper
-// (c) 2005 object networks
-//
-// contains some helper code for glut
-// e.g. entire string printing
-
-#ifndef _glutx_
-#define _glutx_
-void glutxStrokeString(const char* txt, void* font, float x, float y) ;
-char* glutxStrokeStringExpert(const char* txt, void* font);
-void glutxBitmapString(char* txt, void* font,int x, int y) ;
-float glutxBitmapSize(char* txt, void* font);
-float glutxStrokeSize(const char* txt, void* font);
-int glutxNumberOfLines(const char* txt);
-#endif
