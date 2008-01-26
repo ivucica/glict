8d1d1192f3ec108eef2158524ed1fab5da8e5109
zerocoolz <zerocoolz@de5a8d88-efa6-47d7-abed-7a5c0f8920f3>
2008-01-26 14:45:37 +0000

    * font class. * credits. * basic constructors and destructors.

--- src/GLICT/container.h
+++ src/GLICT/container.h
@@ -1,20 +1,20 @@
 /*
-    GLICT2 - Graphics Library Interface Creation Toolkit 2
-    Copyright (C) 2008 Ivan Vucica
+	GLICT2 - Graphics Library Interface Creation Toolkit 2
+	Copyright © 2008 Ivan Vucica & Robbert de Vries
 
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Library General Public
-    License as published by the Free Software Foundation; either
-    version 2 of the License, or (at your option) any later version.
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Library General Public
+	License as published by the Free Software Foundation; either
+	version 2 of the License, or (at your option) any later version.
 
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Library General Public License for more details.
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	Library General Public License for more details.
 
-    You should have received a copy of the GNU Library General Public
-    License along with this library; if not, write to the Free
-    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+	You should have received a copy of the GNU Library General Public
+	License along with this library; if not, write to the Free
+	Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 
 /**
@@ -23,7 +23,6 @@
  * \sa glictContainer
  */
 
-
 #ifndef __GLICT2_CONTAINER_H
 #define __GLICT2_CONTAINER_H
 
@@ -31,9 +30,9 @@
 
 class glictContainer : public glictEventHandler 
 {
-    public:
-        glictContainer();
-        ~glictContainer();
+	public:
+		glictContainer();  // constructor.
+		~glictContainer(); // destructor.
 };
 
 #endif
