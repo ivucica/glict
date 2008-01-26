81f2fa92adcd78431c627cd5d2f2f64e6862dd87
Ivan Vučica <ivan@vucica.net>
2008-01-26 00:14:01 +0000

    * initial creation of glict2 branch

--- glict/examples/memory_sdl/sdlfont.h
+++
@@ -1,7 +0,0 @@
-#ifndef __SDLFONT_H
-#define __SDLFONT_H
-
-void SDLFontDrawChar(char t, SDL_Surface* img, int x1, int y1);
-void SDLFontDraw(const char* txt, const void* font, float x, float y);
-float SDLFontSize(const char* txt, const void* font);
-#endif
