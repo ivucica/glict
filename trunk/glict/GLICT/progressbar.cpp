287b16c800af51f4426484c5bdda1ca795549513
ivucica <ivucica@de5a8d88-efa6-47d7-abed-7a5c0f8920f3>
2008-01-29 16:08:55 +0000

    * fixed progressbar so it finally works now ;)

--- trunk/glict/GLICT/progressbar.cpp
+++ trunk/glict/GLICT/progressbar.cpp
@@ -10,13 +10,13 @@ glictProgressBar::~glictProgressBar () {
 }
 
 void glictProgressBar::Paint() {
-	static glictColor white(1,1,1,1);
+	static glictColor white(.75,.75,.75,.75);
 	static glictColor green(0,1,0,1);
 	glictGlobals.PaintRect(this->x+glictGlobals.translation.x, this->x+this->width+glictGlobals.translation.x,
 		this->y+glictGlobals.translation.y, this->y+this->height+glictGlobals.translation.y, white);
 
-	glictGlobals.PaintRect(this->x+glictGlobals.translation.x +2, this->x+this->width+glictGlobals.translation.x -2,
-		this->y+glictGlobals.translation.y +2,  this->y+this->height+glictGlobals.translation.y -2, green);
+	glictGlobals.PaintRect(this->x+glictGlobals.translation.x +1, this->x+(this->width* percent/100.)+glictGlobals.translation.x  -1,
+		this->y+glictGlobals.translation.y +1,  this->y+this->height+glictGlobals.translation.y -1, green);
 
 
 	this->CPaint();
