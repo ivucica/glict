a8393670986d98acf1732eff1734d141aea92de5
nfries88 <Nate Fries>
2009-05-10 12:49:40 +0000

    * Colors, borders, and BGActiveness on glictProgressbar * Note to ivucica: The scrollchip can't be moved once its past a certain point near the bottom of the scrollbar, but once again I'm probably too retarded to fix it. :D

--- glict/GLICT/progressbar.h
+++ glict/GLICT/progressbar.h
@@ -1,6 +1,7 @@
 #ifndef __GLICT_PROGRESSBAR_H
 #define __GLICT_PROGRESSBAR_H
 #include <GLICT/container.h>
+#include <GLICT/types.h>
 
 class glictProgressBar : public glictContainer {
 	public:
@@ -11,10 +12,18 @@ class glictProgressBar : public glictContainer {
 
 		void SetValue(float newpcnt);
 		void AddValue(float pcnt);
+		void SetBGActiveness(bool active) { bgactive = active; }
+		void SetBGColor(const glictColor& color) { bgColor = color; }
+		void SetFGColor(const glictColor& color) { fgColor = color; }
+		void SetBorderColor(const glictColor& color) { borderColor = color; }
 		float GetValue();
 
 	private:
 		float percent;
+		bool bgactive;
+		glictColor bgColor;
+		glictColor fgColor;
+		glictColor borderColor;
 };
 
 #endif
